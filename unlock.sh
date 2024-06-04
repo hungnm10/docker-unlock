478a5c7558c4f5afbd559c26627bcb448351d694
hungnm10 <hungnm10@gmail.com>
2024-06-05 04:54:22 +0700

    Update unlock.sh
    - Add docker, docker-copose setup

--- unlock.sh
+++ unlock.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
 
+# Check if Docker is installed
+if command -v docker &>/dev/null; then
+    echo "Docker is already installed."
+else
+    echo "Docker is not installed. Proceeding with installations..."
+    # Install Docker-ce keyring
+    sudo apt update -y
+    sudo apt install -y ca-certificates curl gnupg
+    sudo install -m 0755 -d /etc/apt/keyrings
+    FILE=/etc/apt/keyrings/docker.gpg
+    if [ -f "$FILE" ]; then
+        sudo rm "$FILE"
+    fi
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o "$FILE"
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    # Add Docker-ce repository to Apt sources and install
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release; echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update -y
+    sudo apt -y install docker-ce
+fi
+
+# Check if docker-compose is installed
+if command -v docker-compose &>/dev/null; then
+    echo "Docker-compose is already installed."
+else
+    echo "Docker-compose is not installed. Proceeding with installations..."
+
+    # Install docker-compose subcommand
+    sudo apt -y install docker-compose-plugin
+    sudo ln -sv /usr/libexec/docker/cli-plugins/docker-compose /usr/bin/docker-compose
+    docker-compose --version
+fi
+
 # New JSON content to add
 new_mirrors=(
-  "https://dockerhub.timeweb.cloud".
   "https://mirror.gcr.io"
-  "https://daocloud.io"
-  "https://c.163.com"
-  "https://huecker.io"
-  "https://registry.docker-cn.com"
 )
 
 if [ -f /etc/docker/daemon.json ]; then
